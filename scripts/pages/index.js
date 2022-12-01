9f1a1e4d43d843c6e1f498b73717fb14faca35c4
Celine Condoris <celine.condoris@gmail.com>
2022-12-01 19:55:24 +0100

    Add photos in assets + work on fetch with JSON

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,43 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
+        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        fetch('../data/photographers.json')
+        .then(function(response) {
+          // console.log(response.json());
+          return response.json();
+        })
+        .then(function (datas) {
+          console.log(datas.photographers)
+          return ({
+            photographers: datas.photographers})
+        })
+        .catch(function(error) {
+          console.log(error)
+        })
+        // let photographers = [
+        //     {
+        //         "name": "Ma data test",
+        //         "id": 1,
+        //         "city": "Paris",
+        //         "country": "France",
+        //         "tagline": "Ceci est ma data test",
+        //         "price": 400,
+        //         "portrait": "account.png"
+        //     },
+        //     {
+        //         "name": "Autre data test",
+        //         "id": 2,
+        //         "city": "Londres",
+        //         "country": "UK",
+        //         "tagline": "Ceci est ma data test 2",
+        //         "price": 500,
+        //         "portrait": "account.png"
+        //     },
+        // ]
+        // console.log([...photographers]);
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        // return ({
+        //     photographers: [...photographers]})
     }
 
     async function displayData(photographers) {
@@ -41,6 +55,5 @@
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
-    
+
     init();
-    
