de85713aaff432813b749dd6227a9c677c022a7a
Celine Condoris <celine.condoris@gmail.com>
2022-12-14 19:33:05 -0400

    add aria and role attributes on photographer's img

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -11,11 +11,13 @@ function photographerFactory(data) {
 
       const link = document.createElement('a');  //je crée un lien
       // link.setAttribute("href", `photographer.html?${id}`)
-      link.classList.add("photographerPage__link")  // j'ajoute la classe "photographerPage__link"
+      link.classList.add("photographerPage__link");  // j'ajoute la classe "photographerPage__link"
 
       const img = document.createElement('img'); //je crée l'avatar
       img.setAttribute("src", picture);
       img.setAttribute("alt", `Portrait de ${name}`);
+      img.setAttribute("role", "button");
+      img.setAttribute("aria-label", `Accéder à la page de ${name}`);
 
       const photographerName = document.createElement( 'h2' ); //je crée le nom/prénom
       photographerName.classList.add('photographer-name');
@@ -36,7 +38,7 @@ function photographerFactory(data) {
       link.appendChild(img); //j'insère mes éléments créés dans leur emplacement
       link.appendChild(photographerName);
 
-      article.appendChild(link)
+      article.appendChild(link);
       article.appendChild(location);
       article.appendChild(quote);
       article.appendChild(pricePerDay);
