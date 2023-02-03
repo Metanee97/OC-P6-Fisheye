4d64a876333f0911126a90c7f97ea48f9a7b8817
Celine Condoris <celine.condoris@gmail.com>
2023-02-03 14:54:57 -0400

    finish lightbox

--- js/utils/lightbox.js
+++ js/utils/lightbox.js
@@ -1,24 +1,21 @@
 // import { enableBodyScroll, disableBodyScroll } from "./body-scroll-lock";
+const $mainWrapper = document.getElementById('main-wrapper');
 
 class Lightbox {
+
+  static current = null;
 
   static openLightbox(target) {
     const media = target.dataset.href;  //récupère chemin img
     const mediaType = target.dataset.type;  //récupère chemin img
-
-    console.log(target);
+    this.current = target;
     // const id = target.id;
-    // console.log(typeof media);
       new Lightbox(media, mediaType); //crée une lightbox avec chemin img
   }
 
   constructor(media, type) {
     this.element = this.buildDom(media)
-    if(type === 'image') {
-      this.loadMedia(media)
-    } else {
-      this.loadMediaVideo(media)
-    }
+    this.load(media, type);
     this.onKeyUp = this.onKeyUp.bind(this);
     document.body.appendChild(this.element);
     // disableBodyScroll(this.element)
@@ -26,10 +23,22 @@ class Lightbox {
     document.addEventListener('keyup', this.onKeyUp);
   }
 
+  load(media, type) {
+    if(type === 'image') {
+      this.loadMedia(media)
+    } else {
+      this.loadMediaVideo(media)
+    }
+    const lightboxTitle = this.element.querySelector('.media__title');
+    lightboxTitle.innerText = Lightbox.current.parentElement.querySelector('.title-media').innerText;
+
+  }
+
   loadMedia(media) {
     const image = new Image();
     const lightboxContainer = this.element.querySelector('.lightbox__container');
-    lightboxContainer.appendChild(image)
+    lightboxContainer.innerHTML = "";
+    lightboxContainer.appendChild(image);
     image.src = media;
   }
 
@@ -42,10 +51,15 @@ class Lightbox {
     video.setAttribute('controls', '')
 
     const lightboxContainer = this.element.querySelector('.lightbox__container');
+    lightboxContainer.innerHTML = "";
     lightboxContainer.appendChild(video)
     video.src = media;
   }
 
+  /**
+   *
+   * @param {KeyboardEvent} e
+   */
   onKeyUp (e) {
     if (e.key === 'Escape'){
     this.close(e)
@@ -64,26 +78,58 @@ class Lightbox {
       this.element.parentElement.removeChild(this.element);
     }, 500)
     document.removeEventListener('keyup', this.onKeyUp)
+    $mainWrapper.setAttribute('aria-hidden', 'false')
+  };
+
+  next (e) {
+    e.preventDefault()
+    let next = Lightbox.current.closest('.media-item');
+
+    next = next.nextElementSibling; //article suivant si non nul
+
+    if(next == null) {
+      next = Lightbox.current.closest('.photographer-media').firstElementChild;
+    }
+    next = next.querySelector('.media-item__link');
+    Lightbox.current = next;
+    this.load(next.dataset.href, next.dataset.type);
   }
 
-  next (e, target) {
+  prev (e) {
     e.preventDefault()
-    let nextImg = target.nextElementSibling;
+    let prev = Lightbox.current.closest('.media-item');
+
+    prev = prev.previousElementSibling; //article précédent si non nul
+
+    if(prev == null) {
+      prev = Lightbox.current.closest('.photographer-media').lastElementChild;
+    }
+    prev = prev.querySelector('.media-item__link');
+    Lightbox.current = prev;
+    this.load(prev.dataset.href, prev.dataset.type);
   }
 
   // retourne HTMLElement
   buildDom (media) {
     const lightbox = document.createElement('div');
     lightbox.classList.add('lightbox');
+    lightbox.setAttribute('aria-hidden', 'false')
+    lightbox.setAttribute('role', 'dialog');
+    $mainWrapper.setAttribute('aria-hidden', 'true')
+
     lightbox.innerHTML = `
       <button class="lightbox__close">Fermer</button>
       <button class="lightbox__next">Next</button>
       <button class="lightbox__prev">Prev</button>
       <div class="lightbox__container"></div>
       <p class="media__title"></p>
+
     `
     lightbox.querySelector('.lightbox__close').addEventListener('click', this.close.bind(this));
+
     lightbox.querySelector('.lightbox__next').addEventListener('click', this.next.bind(this))
+
+    lightbox.querySelector('.lightbox__prev').addEventListener('click', this.prev.bind(this))
 
 
     return lightbox
